d94f51caa03a435b23b29be6cefff0f38455a53f
Shine192 <htooaungshine789@gmail.com>
2026-09-20 12:49:37 +0700

    connect different level 1,2 and 3 to main menu
    connect different level 1,2 and 3 to main menu

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void GW1_level_1()
+    {
+        SceneManager.LoadScene("GW1_Level1");
+    }
+
+    public void GW1_level_2()
+    {
+        SceneManager.LoadScene("GW1_Level2");
+    }
+     public void GW1_level_3()
+    {
+        SceneManager.LoadScene("GW1_Level3");
+    }
+
+    public void GW2_level_1()
+    {
+        SceneManager.LoadScene("GW2_Level1");
+    }
+
+    public void GW2_level_2()
+    {
+        SceneManager.LoadScene("GW2_Level2");
+    }
+     public void GW2_level_3()
+    {
+        SceneManager.LoadScene("GW1_Level3");
+    }
+
+    public void GW3_level1()
+    {
+        SceneManager.LoadScene("GW3_Level1");
+    }
+
+    public void GW3_level2()
+    {
+        SceneManager.LoadScene("GW3_Level2");
+    }
+
+    public void GW3_level3()
+    {
+        SceneManager.LoadScene("GW3_Level3");
+    }
+}
