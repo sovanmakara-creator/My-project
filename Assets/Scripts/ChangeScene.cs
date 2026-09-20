6f5e5b4936f01f249d19b865fb333a1896ac6a75
Shine192 <htooaungshine789@gmail.com>
2026-09-20 11:45:22 +0700

    update on sept20 on main

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -1,7 +1,24 @@
 using UnityEngine;
+<<<<<<< Updated upstream
 using UnityEngine.Scene
 
 public class ChangeScene : MonoBehaviour
 {
    
+=======
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+>>>>>>> Stashed changes
 }
